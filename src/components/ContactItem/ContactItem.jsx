ef56f178421c594d574bae1d39a27fd73d8e4f49
roman_orlov <roman.developer@mail.ru>
2022-03-14 16:24:13 +0500

    Added json integration

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,7 +1,9 @@
 import Note from "../Note/Note";
 import styles from "./ContactItem.module.scss";
 
-const ContactItem = ({ title, notes, isActived }) => {
+const ContactItem = ({ title, notes }) => {
+    const isActived = false;
+
     return (
         <button className={`${styles["contact-item"]} ${isActived && styles["contact-item_actived"]}`}>
             <h2 className={styles["contact-item__title"]}>{title}</h2>
--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,11 +1,20 @@
+import ContactItem from "../../components/ContactItem/ContactItem";
 import styles from "./Contacts.module.scss";
 
+import stateInfo from "../../API/state.json";
+
 const Contacts = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+	const pickPoints = stateInfo.pickPoints;
+
+	return (
+		<div className={styles["contact-page"]}>
+			<div className={styles["contact-page__container"]}>
+				<div className={styles["contact-page__item-box"]}>
+					{pickPoints.map( ( item, index ) => <ContactItem title={item.address} notes={item.budgets} key={index} /> )}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Contacts;
