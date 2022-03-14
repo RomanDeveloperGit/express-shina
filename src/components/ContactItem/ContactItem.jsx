a887dbbff7c1b46f77f137ce997af6225ba9c3b4
roman_orlov <roman.developer@mail.ru>
2022-03-14 16:09:09 +0500

    Added contact item

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,9 +1,13 @@
+import Note from "../Note/Note";
 import styles from "./ContactItem.module.scss";
 
-const ContactItem = () => {
+const ContactItem = ({ title, notes, isActived }) => {
     return (
-        <div>
-            
+        <div className={`${styles["contact-item"]} ${isActived && styles["contact-item_actived"]}`}>
+            <h2 className={styles["contact-item__title"]}>{title}</h2>
+            <div className={styles["contact-item__note-box"]}>
+                {notes.map( ( item, index ) => <Note key={index}>{item}</Note> )}
+            </div>
         </div>
     );
 };
