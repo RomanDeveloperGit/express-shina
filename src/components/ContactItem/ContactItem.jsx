99d7808816032506246d3184fdd3a931c7b93afe
roman_orlov <roman.developer@mail.ru>
2022-03-14 16:13:37 +0500

    Fix contact item component

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -3,12 +3,12 @@ import styles from "./ContactItem.module.scss";
 
 const ContactItem = ({ title, notes, isActived }) => {
     return (
-        <div className={`${styles["contact-item"]} ${isActived && styles["contact-item_actived"]}`}>
+        <button className={`${styles["contact-item"]} ${isActived && styles["contact-item_actived"]}`}>
             <h2 className={styles["contact-item__title"]}>{title}</h2>
             <div className={styles["contact-item__note-box"]}>
                 {notes.map( ( item, index ) => <Note key={index}>{item}</Note> )}
             </div>
-        </div>
+        </button>
     );
 };
 
